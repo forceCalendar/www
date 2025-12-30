541f6f0c4cdb54311466c0a52e65778a7c9bf9dc
thedhanawada <wep.proto@gmail.com>
2025-12-30 22:41:53 +1100

    Remove deprecated swcMinify option from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* Optimize for production */
   reactStrictMode: true,
-  swcMinify: true,
 };
 
 export default nextConfig;
